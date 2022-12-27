90511339480fef5eb2c82aaa4086061ac1aaa8e9
karol <lolis103@gmail.com>
2022-12-28 00:49:46 +0100

    finished - 4rd level of details added.

--- BoxRoom.php
+++ BoxRoom.php
@@ -1,7 +1,8 @@
 <?php
 
 
-    class BoxRoom{
+    class BoxRoom
+{
 
     public $amount_of_prisoners;
     public $amount_of_boxes;
@@ -11,7 +12,9 @@
     public $amount_of_simulations;
     public $describe=0;
     public $describe2=0;
-    public $describe3=1;
+    public $describe3=0;
+    public $describe4=0;
+
 
 
     function __construct($amount, $simulations)
@@ -37,68 +40,90 @@
 
         for ($i=1;$i<=$this->allowed_attemps_for_prisoner; $i++) //ammount of attempts
         {   
-            if ($toCheck==null){
+            if ($toCheck==null)
+            {
                 $toCheck=$id;
             }
             if ($this->describe==1) {echo "wiezien $id w pudelku $toCheck znalazl ".$this->shuffledBoxes[$toCheck].'<br>';}
             $toCheck=$this->shuffledBoxes[$toCheck];
 
 
-            if ($this->shuffledBoxes[$toCheck]==$id){
+            if ($this->shuffledBoxes[$toCheck]==$id)
+            {
                 $success=1;
                 if ($this->describe==1) {echo 'więzień '.$id.' w pudełku '. $toCheck. ' znalazł '.$this->shuffledBoxes[$toCheck].". To była jego $i próba <hr>";}
                 return 1;
                 break;
             }
-            if ($i==$this->allowed_attemps_for_prisoner && $success==false){
+            if ($i==$this->allowed_attemps_for_prisoner && $success==false)
+                {
                 if ($this->describe==1) {echo  "nie udało sie, więzień $id ginie po $i probach <hr>";}
                 return 0;
-               break;
-            }
-            if ($i==$this->allowed_attemps_for_prisoner && $success==true && $id==$this->amount_of_prisoners){
+                break;
+                }
+                if ($i==$this->allowed_attemps_for_prisoner && $success==true && $id==$this->amount_of_prisoners)
+                {
                 if ($this->describe==1) {echo '<h1>brawo, wygrywasz</h1>';}
                 return 1;
                 break;
             }
-            
+                
         }
     }
 
-    public function CheckResult(){
+    public function CheckPopulation()
+    {
+        foreach ($this->queue as $checker)
+        {
+            if ($this->CheckMyBoxes($checker)!==1) 
+            {
+            if ($this->describe2==1 ||$this->describe3==1){echo "<h5>prisoner $checker  has failed- all population has failed</h5>";}
+            return 0;
+            break;
+            } 
+                        
+            if ($this->CheckMyBoxes($checker)==1) 
+            {
+                if ($this->describe2==1){echo "<h5>$checker succeed</h5>";}   
+            }
+            if ($this->CheckMyBoxes($checker)==1 && $checker==100) 
+            {
+                if ($this->describe3==1){echo "<h3>Population has succeed</h3>";}
+                return 1;
+            }    
+        }
+    }
+    public function repeatTheTest()
+    {
+        $counter=0;
+        for ($i=1;$i<=$this->amount_of_simulations;$i++)
+        {
+            if ($this->describe4==1) {
+            echo "<h1>simulation $i</h1>";
+            Var_dump($this->shuffledBoxes);
+
+            }
+        $result=$this->CheckPopulation();
+        if ($result==1){$counter=$counter+1;}
+            
+            
+        shuffle($this->shuffledBoxes);//randomizes the boxes order
+        array_unshift($this->shuffledBoxes, "added zero index");
+        unset($this->shuffledBoxes[0]); 
+        }
+        $part=$counter/$this->amount_of_simulations;
+        echo"<h1>po wykonaniu $this->amount_of_simulations testów, powodzeniem zakończyło się $counter z nich. <br>Procent testów zakończonych powodzeniem:$part</h1>";
 
     }
-
-
 };
 
 
-$simulation = new BoxRoom(100, 100);
-echo "<h1>Simulation </h1>";
-Var_dump($simulation->shuffledBoxes);
-echo '<hr>';
+$simulation = new BoxRoom(100, 1000); //arg - number of prisoners, number of simulations.
 
-// check the population within a single simulation
 
-foreach ($simulation->queue as $checker){
+$result = $simulation->repeatTheTest();
 
-    if ($result=$simulation->CheckMyBoxes($checker)!==1) {
-        if ($simulation->describe2==1 ||$simulation->describe3==1){echo "<h5>prisoner $checker  has failed- all population has failed</h5>";}
-        return 0;
-        break;
-    } 
-    
-    if ($result=$simulation->CheckMyBoxes($checker)==1) {
-        
-        if ($simulation->describe2==1){echo "<h5>$checker succeed</h5>";}   
-    }
-    if ($result=$simulation->CheckMyBoxes($checker)==1 && $checker==100) {
-        
-        if ($simulation->describe3==1){echo "<h3>Population has succeed</h3>";}
-        return 1;
 
-    } 
-    
-}
 
 
 
